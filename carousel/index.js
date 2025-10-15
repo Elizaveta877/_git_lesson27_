cf29068da2378156a54d4825abd49d1be499fd8e
Elizaveta877 <ep50384@gmail.com>
2025-10-15 21:55:17 +0300

    add

--- carousel/index.js
+++ carousel/index.js
@@ -1,2 +1,43 @@
+// DOM
 const slides = document.querySelectorAll('.slide');
+const pauseBtn = document.querySelector('#pause-btn')
+const previosBtn = document.querySelectorAll('#previous-btn')
+const nextBtn = document.querySelectorAll('#next-btn')
+// const
+
+const SLIDE_COUNT = slides.length
+const TIMER_INTERVAL = 2000
+
+// Variables
 let currentSlide = 0 
+let timerId = null
+let isPlaying = true
+
+
+function nextSlide() {
+ slides[currentSlide].classList.toggle('active')
+ currentSlide = (currentSlide + 1) % SLIDE_COUNT
+slides[currentSlide].classList.toggle('active')
+}
+
+timerId = setInterval(nextSlide, TIMER_INTERVAL)
+
+function pauseHandler() {
+  pauseBtn.innerHTML = 'Play'
+clearInterval(timerId)
+isPlaying = !isPlaying
+}
+
+function playHandler() {
+pauseBtn.innerHTML = 'Pause'
+isPlaying = !isPlaying
+  timerId = setInterval(nextSlide, TIMER_INTERVAL)
+}
+
+function togglePlayHandler() {
+ isPlaying ? pauseHandler(): playHandler()
+}
+
+pauseBtn.addEventListener('click', togglePlayHandler)
+previosBtn.addEventListener('click, previousSlide')
+nextBtn.addEventListener('click', nextSlide)
