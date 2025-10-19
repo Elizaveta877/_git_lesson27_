7befcd869785fc47ba839f0895b14b307e3ec54e
Elizaveta877 <ep50384@gmail.com>
2025-10-20 02:15:32 +0300

    add

--- carousel/index.js
+++ carousel/index.js
@@ -1,6 +1,7 @@
+
 // DOM
 const container = document.querySelector('#carousel')
-// const slidesContainer = container.querySelector('#slides-container')
+const slidesContainer = container.querySelector('#slides-container')
 const slides = container.querySelectorAll('.slide');
 const indicatorsContainer = container.querySelector('#indicators-container')
 const indicators = container.querySelectorAll('.indicator')
@@ -9,19 +10,23 @@ const previousBtn = container.querySelector('#previous-btn')
 const nextBtn = container.querySelector('#next-btn')
 // const
 
+
 const SLIDE_COUNT = slides.length
-const TIMER_INTERVAL = 2000
+const CODE_SPACE = 'Space'
+const COSE_ARROW_LEFT = 'ArrowLeft'
+const CODE_ARROW_RIGHT = 'ArrowRight'
 const FA_PAUSE = '<i class="fa-solid fa-pause"></i>'
 const FA_PLAY = '<i class="fa-solid fa-play"></i>'
 // const FA_PREV = '<i class="fa-solid fa-chevron-left"></i>'
 // const FA_NEXT = '<i class="fa-solid fa-chevron-right"></i>'
-
-
+const TIMER_INTERVAL = 2000
+const SWIPE_TRESHOLD = 100
 // Variables
 let currentSlide = 0 
 let timerId = null
 let isPlaying = true
-let currentSlideBackground =  null
+let swipStartX = null
+let swipeEndX = null 
 
 function gotoNth(n) {
  slides[currentSlide].classList.toggle('active')
@@ -32,9 +37,8 @@ function gotoNth(n) {
 slides[currentSlide].classList.toggle('active')
  indicators[currentSlide].classList.toggle('active')
 //  Get background color
- const computedStyle = window.getComputedStyle(slides[currentSlide])
- currentSlideBackground = computedStyle.background
- indicators[currentSlide].style.background = currentSlideBackground
+
+ indicators[currentSlide].style.background = window.getComputedStyle(slides[currentSlide]).background
 }
 
 
@@ -84,24 +88,55 @@ function indicatorClickHandler(e) {
 const {target} = e
 if (target && target.classList.contains('indicator')) {
   pauseHandler()
-  gotoNth(+target.dataSet.slideTo)
+  gotoNth(+target.dataset.slideTo)
 }
 }
 
 function keydownHandler(e) {
   const code = e.code
-  console.log(code)
 
-  if (code === 'Space') togglePlayHandler()
-  if (code === 'ArrowLefr') prevHandler()
-  if (code === 'ArrowRight') nextHandler()
+  if (code === COSE_ARROW_LEFT) prevHandler()
+  if (code === CODE_ARROW_RIGHT) nextHandler()
+      if (code === CODE_SPACE) {
+        e.preventDefault()
+        togglePlayHandler()
+      }
+}
+
+function swipeStartHandler(e) {
+swipeStartX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+}
+
+function swipeEndHandler(e) {
+  swipeEndX = e instanceof MouseEvent ? e.clientX : e.changedTouches[0].clientX
+
+
+const diff = swipStartX - swipeEndX
+
+if (diff > SWIPE_TRESHOLD) prevHandler()
+
+  if (diff < -SWIPE_TRESHOLD) nextHandler()
 }
 
 
+function initEventListeners() {
 pauseBtn.addEventListener('click', togglePlayHandler)
 previousBtn.addEventListener('click', prevHandler)
 nextBtn.addEventListener('click', nextHandler)
 indicatorsContainer.addEventListener('click', indicatorClickHandler)
 document.addEventListener('keydown', keydownHandler)
+slidesContainer.addEventListener('touchstart', swipeStartHandler, {passive: true})
+slidesContainer.addEventListener('mousdown', swipeStartHandler)
+slidesContainer.addEventListener('touchend', swipeEndHandler)
+slidesContainer.addEventListener('mouseup', swipeEndHandler)
+}
 
+function init() {
+initEventListeners()
 tick()
+}
+
+
+init()
+
+
