8f7c1c2423d2d45f7c3ff41ac49a0c84cc1de6cd
Elizaveta877 <ep50384@gmail.com>
2025-10-20 00:00:12 +0300

    add

--- carousel/index.js
+++ carousel/index.js
@@ -2,7 +2,7 @@
 const container = document.querySelector('#carousel')
 // const slidesContainer = container.querySelector('#slides-container')
 const slides = container.querySelectorAll('.slide');
-// const indicatorsContainer = container.querySelector('#indicators-container')
+const indicatorsContainer = container.querySelector('#indicators-container')
 const indicators = container.querySelectorAll('.indicator')
 const pauseBtn = container.querySelector('#pause-btn')
 const previousBtn = container.querySelector('#previous-btn')
@@ -10,7 +10,7 @@ const nextBtn = container.querySelector('#next-btn')
 // const
 
 const SLIDE_COUNT = slides.length
-const TIMER_INTERVAL = 1000
+const TIMER_INTERVAL = 2000
 const FA_PAUSE = '<i class="fa-solid fa-pause"></i>'
 const FA_PLAY = '<i class="fa-solid fa-play"></i>'
 // const FA_PREV = '<i class="fa-solid fa-chevron-left"></i>'
@@ -21,14 +21,20 @@ const FA_PLAY = '<i class="fa-solid fa-play"></i>'
 let currentSlide = 0 
 let timerId = null
 let isPlaying = true
-
+let currentSlideBackground =  null
 
 function gotoNth(n) {
  slides[currentSlide].classList.toggle('active')
  indicators[currentSlide].classList.toggle('active')
+
+ indicators[currentSlide].style.background = null
  currentSlide = (n + SLIDE_COUNT) % SLIDE_COUNT
 slides[currentSlide].classList.toggle('active')
  indicators[currentSlide].classList.toggle('active')
+//  Get background color
+ const computedStyle = window.getComputedStyle(slides[currentSlide])
+ currentSlideBackground = computedStyle.background
+ indicators[currentSlide].style.background = currentSlideBackground
 }
 
 
@@ -74,8 +80,28 @@ function prevHandler() {
   pauseHandler()
 }
 
+function indicatorClickHandler(e) {
+const {target} = e
+if (target && target.classList.contains('indicator')) {
+  pauseHandler()
+  gotoNth(+target.dataSet.slideTo)
+}
+}
+
+function keydownHandler(e) {
+  const code = e.code
+  console.log(code)
+
+  if (code === 'Space') togglePlayHandler()
+  if (code === 'ArrowLefr') prevHandler()
+  if (code === 'ArrowRight') nextHandler()
+}
+
+
 pauseBtn.addEventListener('click', togglePlayHandler)
-previousBtn.addEventListener('click', gotoPrev)
-nextBtn.addEventListener('click', gotoNext)
+previousBtn.addEventListener('click', prevHandler)
+nextBtn.addEventListener('click', nextHandler)
+indicatorsContainer.addEventListener('click', indicatorClickHandler)
+document.addEventListener('keydown', keydownHandler)
 
 tick()
